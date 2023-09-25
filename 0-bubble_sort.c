5c58c5f215882a399d4ed5da4e368026be2f20fc
Odwori1 <odwori.okuku@gmail.com>
2023-09-25 12:42:35 -0700

    function sorts a doubly linked list of integers

---
+++ 0-bubble_sort.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * swap - Funct that swaps two integers.
+ * @a: first integer to swap.
+ * @b: second integer to swap.
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: The array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
